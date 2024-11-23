73d8c840832ab25410e6c262f1ba1ad402962b91
miwanoff <marina.iva.noffa@gmail.com>
2024-11-23 12:46:41 +0200

    Add while loop to csv

--- csv.php
+++ csv.php
@@ -1,5 +1,19 @@
 <?php
 
-$file = fopen("contacts.csv", "r");
-print_r(fgetcsv($file));
-fclose($file);
+// $file = fopen("contacts.csv", "r");
+// print_r(fgetcsv($file));
+// fclose($file);
+
+$row = 1;
+if (($handle = fopen("contacts.csv", "r")) !== false) {
+    while (($data = fgetcsv($handle, 1000)) !== false) {
+        print_r($data);
+        // $num = count($data); // количество полей в строке
+        // for ($c = 0; $c < $num; $c++) {
+        //     echo $data[$c] . "\n";
+        // }
+    }
+    fclose($handle);
+} else {
+    echo "Can`t open file";
+}
